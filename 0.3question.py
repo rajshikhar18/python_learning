17d6173604bcf056b3cbec37b57fa200df49e122
rajshikhar18 <rajshikharsoni@gmail.com>
2026-07-31 02:52:36 +0530

    logic clear

---
+++ 0.3question.py
@@ -0,0 +1,14 @@
+string = input("Enter the string:")
+char_frequency ={}
+for char in string:
+    if char in char_frequency:
+        char_frequency[char] +=1
+    else:
+        char_frequency[char] =1
+
+for char in string:
+    if char_frequency[char] >1:
+        print(f"'{char}' is the first repeating character in the string")
+        break 
+    else:
+        print("No repeating character found in the string")        
