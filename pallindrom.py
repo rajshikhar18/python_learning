4887cc807977b649d8a8264382e0b70526ec9c63
rajshikhar18 <rajshikharsoni@gmail.com>
2026-07-30 13:10:27 +0530

    Update pallindrom.py

--- pallindrom.py
+++ pallindrom.py
@@ -1,9 +1,9 @@
 list1 =[ 1,2,3,4,5,6,7,8,9,10]
-list2 = [11,12,13,14,15,16,17,18,19,20]
+
 copy_list1 =list1.copy()
 copy_list1.reverse()
 
-if copy_list1 == list2:
+if copy_list1 == list1:
     print("its a pallindrom :")
 else: 
     print("its not a pallindrom :")
