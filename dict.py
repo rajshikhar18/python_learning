a12a128073a4def2face56a86de18982bafdc53f
rajshikhar18 <rajshikharsoni@gmail.com>
2026-07-30 23:09:53 +0530

    lecture 2

---
+++ dict.py
@@ -0,0 +1,28 @@
+#dict = {
+#    "name ": "Rajesh",
+#    "age ": 20, 
+
+#}
+##print(dict)
+##nested dictionary 
+
+students = {
+    "name": "raj",
+    "age": 29 ,
+    "subjects" :{
+        "maths" : 70,
+        "chem" : 50,
+        "phy" : 52, 
+        
+
+    }
+}
+print(students)
+print(students["subjects"])
+print(students["subjects"]["maths"])
+print(students.keys())
+print(students.values())
+print(students.items())
+print(students.get("name"))
+students.update({"name" : "lucky"})
+print(students)
---
+++ set.py
@@ -0,0 +1,15 @@
+#collection = { "rs",  2, 3 , "a", "b", "c" }
+#set =set()
+#print(type(set))
+#print(len(collection))
+
+
+#print(collection.pop())
+#print(collection.pop())
+#print(collection.pop())
+
+set1 ={"python", "java", "c++", "python", "javascript"}
+set2 ={"java", "python", "java", "c++", "c"}
+set3 =set1.union(set2)
+print(set3)
+print("required number of classroom:", len(set3))
