c8c111db7b4f82243242ed047133d8e260dadb7a
rajshikhar18 <rajshikharsoni@gmail.com>
2026-07-30 13:04:01 +0530

    lecture 1

---
+++ lecture1.py
@@ -0,0 +1,7 @@
+
+str = "apna college"
+print(str[0:4])  # Output: apna
+print(str.find("o"))  # Output: 4
+#name =input("Enter your name:")
+#print("length of name is :" , len(name))
+print(str.count("e"))
---
+++ list.py
@@ -0,0 +1,10 @@
+mov1 = input("Enter the first movie name:")
+mov2 = input("Enter the second movie name:")
+mov3 = input("Enter the third movie name:")
+movList = []
+
+movList.append(mov1)
+movList.append(mov2)
+movList.append(mov3)
+
+print("The list of movies is:", movList)
